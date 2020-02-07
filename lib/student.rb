f7b46869b01c26270d8f624b8a9eff5fa5d23029
Stephen Sanchez <stephensanchez05@gmail.com>
2020-02-07 00:56:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,16 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :id, :name, :grade
+  
+  def initialize(id = nil, name, grade)
+    @name = name
+    @grade = grade
+  end
+  
+  def self.create_table
+    
 
 
 end
